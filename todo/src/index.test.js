deb92c85a9cd881a65e68b82fea23551ff92e164
ulldecorb <ausias.bertran@gmail.com>
2021-10-06 23:49:38 +0200

    Add header component

--- todo/src/index.test.js
+++ todo/src/index.test.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import index from './index';
 
 test('when main index', () => {
   render(index);
-  const renderApp = screen.getByTag('App');
-  expect(renderApp).toBeInTheDocument();
+  const dom = jest.fn(ReactDOM);
+  expect(dom).toBeInTheDocument();
 });
