112ec9b0ec9aae37f814c8c858c7fd508850dbe1
ulldecorb <ausias.bertran@gmail.com>
2021-10-08 15:09:05 +0200

    Refactor form on new task component

--- todo/src/components/list/index.jsx
+++ todo/src/components/list/index.jsx
@@ -1,19 +1,39 @@
+// import { useEffect, useState } from 'react';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import { loadTasks, deleteTask } from '../../redux/actions/actioncCreators';
+// import { loadTasks, deleteTask, updateTask } from '../../redux/actions/actionCreators';
+import { loadTasks, deleteTask } from '../../redux/actions/actionCreators';
 import AddTask from '../newtask/index';
 
-function List({ tasks, dispatch }) {
+function List({ tasks = [], dispatch }) {
+  // const [task, setTask] = useState('');
   useEffect(() => {
     if (!tasks.lenght) dispatch(loadTasks());
   }, []);
+  // console.log(task);
+
+  // function getUpdatedtask() {
+  //   dispatch(updateTask());
+  // }
 
   return (
     <section className="list">
       <ul className="list__box">
         {tasks.map((item) => (
+
           <li className="list__task">
+            {/* <input
+              className="list__input"
+              id="list"
+              type="text"
+              value={item.task}
+              autocComplete="off"
+              maxLength="20"
+              placeholder={task}
+              onChange={(event) => setTask(event.target.value)}
+            /> */}
+
             <p className="task__title">
               {item.task}
             </p>
