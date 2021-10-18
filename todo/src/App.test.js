1acf1e3dcf96169bb41e6a8786d49261675bf6ac
ulldecorb <ausias.bertran@gmail.com>
2021-10-18 21:36:24 +0200

    Remove failed test

--- todo/src/App.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const header = screen.getElementsByTagName('Header')[0];
-  expect(header).toBeInTheDocument();
-});
